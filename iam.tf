342363614bb3fd885d789af18b182e6821548236
danimal141 <hideaki.ishii1204@gmail.com>
2020-09-09 16:40:20 +0900

    Avoid deprecated way

--- iam.tf
+++ iam.tf
@@ -19,12 +19,12 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "eks-cluster" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role = "${aws_iam_role.eks-master.name}"
+  role = aws_iam_role.eks-master.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks-service" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role = "${aws_iam_role.eks-master.name}"
+  role = aws_iam_role.eks-master.name
 }
 
 # EKS node
@@ -49,20 +49,20 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "eks-worker-node" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role = "${aws_iam_role.eks-node.name}"
+  role = aws_iam_role.eks-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks-cni" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role = "${aws_iam_role.eks-node.name}"
+  role = aws_iam_role.eks-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "ecr-readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = "${aws_iam_role.eks-node.name}"
+  role = aws_iam_role.eks-node.name
 }
 
 resource "aws_iam_instance_profile" "eks-node" {
   name = "eks-node-profile"
-  role = "${aws_iam_role.eks-node.name}"
+  role = aws_iam_role.eks-node.name
 }
