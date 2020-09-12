5c011a166b6927c051d4552223a0c0afa3c1e837
danimal141 <hideaki.ishii1204@gmail.com>
2020-09-12 12:02:08 +0900

    Add outputs

---
+++ outputs.tf
@@ -0,0 +1,51 @@
+locals {
+  kubeconfig = <<CONFIG
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.cluster.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.cluster.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-1"
+        - ${local.cluster_name}
+CONFIG
+
+  eks_configmap = <<CONFIGMAP
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.eks-node.arn}
+      username: system:node{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAP
+}
+
+output "kubectl_config" {
+  value = local.kubeconfig
+}
+
+output "eks_configmap" {
+  value = local.eks_configmap
+}
